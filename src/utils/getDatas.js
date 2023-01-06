cbb24c689bd300bdadcf46b03615078bc65dda45
Anthony.M <marcellin.anthony77@gmail.com>
2023-01-06 01:48:03 +0100

    Refactoristion de getDatas.js

--- src/utils/getDatas.js
+++ src/utils/getDatas.js
@@ -14,6 +14,27 @@ class NoIdentifiant extends Error {
 }
 
 /**
+ *
+ * @param {Error} error An Object transmitted by the catch instruction
+ * @param {string} personalisedMessage Message written for details
+ * @returns {JSX.Element} An HTML Code Showed in page Content
+ */
+function getErrorHTML(error, personalisedMessage) {
+    return (
+        <>
+            <h2>La requête à échoué, et n'a pas pu être complétée</h2>
+            <p>{personalisedMessage}</p>
+            <h3>Détails de l'erreur :</h3>
+            <ul>
+                <li>Type de l'erreur : {error.name}</li>
+                <li>Code : {error.code}</li>
+                <li>Message: {error.message}</li>
+            </ul>
+        </>
+    );
+}
+
+/**
  * Function called Axios Library to make an request with API user endpoint : /user
  * @param {string} id User identifiant with number
  * @returns {Promise<string|any>}
@@ -27,47 +48,13 @@ export default async function fetchUserData(id) {
         return request.data;
     } catch (error) {
         if (error.code === 'ERR_NETWORK') {
-            return (
-                <>
-                    <h2>La requête à échoué, et n'a pas pu être completée</h2>
-                    <p>Vérifier si le serveur est bien lancée en Back-end !</p>
-
-                    <h3>Détails de l'erreur :</h3>
-                    <ul>
-                        <li>Type de l'erreur : {error.name}</li>
-                        <li>Code : {error.code}</li>
-                        <li>Message: {error.message}</li>
-                    </ul>
-                </>
-            )
+            return getErrorHTML(error, 'Vérifier si le serveur est bien lancée en Back-end !');
         }
         else if (error.code === 'ERR_BAD_REQUEST') {
-            return (
-                <>
-                    <h2>La requête à échoué, et n'a pas pu être completée</h2>
-                    <p>Vérifier l'<u>URL</u> ou l'<u>id</u> envoyée ! Qui dois être conforme à <strong>/user/:id</strong></p>
-                    <h3>Détails de l'erreur :</h3>
-                    <ul>
-                        <li>Type de l'erreur : {error.name}</li>
-                        <li>Code : {error.code}</li>
-                        <li>Message: {error.message}</li>
-                    </ul>
-                </>
-            )
+            return getErrorHTML(error, 'Vérifier l\'<u>URL</u> ou l\'<u>id</u> envoyée ! Qui doit être conforme à <strong>/user/:id</strong>');
         }
         else if ( error.code === 'ERR_NO_IDENTIFIANT') {
-            return (
-                <>
-                    <h2>La requête à échoué, et n'a pas pu être completée</h2>
-                    <p>Veuillez transmettre un identifiant dans la requête ! Qui dois être conforme à <strong>/user/:id</strong></p>
-                    <h3>Détails de l'erreur :</h3>
-                    <ul>
-                        <li>Type de l'erreur : {error.name}</li>
-                        <li>Code : {error.code}</li>
-                        <li>Message: {error.message}</li>
-                    </ul>
-                </>
-            )
+            return getErrorHTML(error, 'Veuillez transmettre un identifiant dans la requête ! Qui doit être conforme à <strong>/user/:id</strong>');
         }
         else {
             console.log(error);
